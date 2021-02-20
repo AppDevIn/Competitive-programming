5dd05f0790d2bb9678391ccf1c5269bd34fcaed6
AppDevIn <teamprojectlive@gmail.com>
2021-02-20 12:42:29 +0800

    Version 05 time limit at test case 57

--- LeetCode-Contest/fed2020/week03/minRemoveToMakeVaild.py
+++ LeetCode-Contest/fed2020/week03/minRemoveToMakeVaild.py
@@ -3,25 +3,23 @@ def minRemoveToMakeValid(s):
     :type s: str
     :rtype: str
     """
-
-    curr = 0
-    fast = 0
+    openBracket = []
+    closeBracket = []
 
     s = [c for c in s]
-    remove = []
-    while(fast < (len(s)) and curr < len(s)):
 
-        if(s[curr] == "(" and s[fast] == ")" and curr < fast):
-            remove.append(curr)
-            remove.append(fast)
-            curr += 1
-            fast += 1
-        elif(s[curr] != "("):
-            curr += 1
-        elif(curr > fast):
-            fast += 1
-        elif(fast != len(s) and s[fast] != ")"):
-            fast += 1
+    for index in range(len(s)):
+        if (s[index] == "("):
+            openBracket.append(index)
+        elif (s[index] == ")" and len(openBracket) > len(closeBracket)):
+            closeBracket.append(index)
+
+    minValue = min(len(openBracket), len(closeBracket))
+
+    remove = openBracket + closeBracket
+    for i in range(0, minValue):
+        remove.append(openBracket[i])
+        remove.append(closeBracket[i])
 
     for i in range(len(s)):
         if((s[i] == "(" or s[i] == ")") and (i not in remove)):
@@ -30,5 +28,5 @@ def minRemoveToMakeValid(s):
     return "".join(s)
 
 
-print("🚀 ~ file: minRemoveToMakeVaild.py ~ line 21 ~ minRemoveToMakeValid('(a(b(c)d)'')",
-      minRemoveToMakeValid("(a(b(c)d)"))
+print("🚀 ~ file: minRemoveToMakeVaild.py ~ line 21 ~ minRemoveToMakeValid('(a(b(c)d))",
+      minRemoveToMakeValid("lee(t(c)o)de)"))
