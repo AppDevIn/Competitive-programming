56c01f120b29a7c2acbd0f94d66f0ff793300801
AppDevIn <teamprojectlive@gmail.com>
2021-02-22 13:39:14 +0800

    Online version

--- LeetCode-Contest/fed2020/week03/brokenCalculator.py
+++ LeetCode-Contest/fed2020/week03/brokenCalculator.py
@@ -1,25 +1,4 @@
 
-def recursive(X, Y, count, up , memeroization=[]):
-
-    if up:
-        if X > Y + 2 or X <= 0 or X in memeroization:
-            return 100
-        elif X == Y:
-            return count
-        
-        count += 1
-        memeroization.append(X)
-        value = recursive(X*2, Y, count, up, memeroization)
-        value2 = recursive(X-1, Y, count, up, memeroization)
-        
-        return min(value, value2)
-        
-    else: 
-        
-        return (X-Y) - 1 
-
-
-
     
 
 
@@ -29,8 +8,12 @@ def brokenCalc(X, Y):
     :type Y: int
     :rtype: int
     """
-    value = recursive(X, Y, 0 , X < Y , [])
-    print("🚀 ~ file: brokenCalculator.py ~ line 33 ~ value", value)
-    
+    if X > Y: return X - Y
+    if X == Y: return 0
+    if Y % 2 == 0:
+        return self.brokenCalc(X, Y//2) + 1
+    else:
+        return self.brokenCalc(X, Y + 1) + 1
+
 
 brokenCalc(1, 1000000000)
