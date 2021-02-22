049cd09bae5a06451c494944d2be5fbda8d53188
AppDevIn <teamprojectlive@gmail.com>
2021-02-22 13:39:26 +0800

    Online version

--- LeetCode-Contest/fed2020/week03/brokenCalculator.py
+++ LeetCode-Contest/fed2020/week03/brokenCalculator.py
@@ -11,9 +11,9 @@ def brokenCalc(X, Y):
     if X > Y: return X - Y
     if X == Y: return 0
     if Y % 2 == 0:
-        return self.brokenCalc(X, Y//2) + 1
+        return brokenCalc(X, Y//2) + 1
     else:
-        return self.brokenCalc(X, Y + 1) + 1
+        return brokenCalc(X, Y + 1) + 1
 
 
 brokenCalc(1, 1000000000)
