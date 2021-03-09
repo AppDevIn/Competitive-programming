504935074f4fa3a231b2dc56e1fe0797bcccdaf8
AppDevIn <teamprojectlive@gmail.com>
2021-03-09 10:27:46 +0800

    1332. Remove Palindromic Subsequences

---
+++ LeetCode-Challenge/march2020/week02/01.py
@@ -0,0 +1,12 @@
+# 1332. Remove Palindromic Subsequences
+# https://leetcode.com/problems/remove-palindromic-subsequences/
+class Solution:
+    def removePalindromeSub(self, s: str) -> int:
+        
+        if len(s) == 0: return 0
+        elif s == s[::-1]: return 1
+        else: return 2
+
+s = Solution()
+
+s.removePalindromeSub("bbaabaaa")
