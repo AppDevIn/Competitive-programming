03f1d64e5e2691e23399538ae06e2e4ce0462d3f
AppDevIn <teamprojectlive@gmail.com>
2023-04-23 12:51:14 +0800

    26. Remove Duplicates from Sorted Array

---
+++ LeetCode-Problem/26. Remove Duplicates from Sorted Array/solution.py
@@ -0,0 +1,31 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+
+        if not list1 and not list2:
+            return list1
+        elif not list1:
+            return list2
+        elif not list2:
+            return list1
+        
+
+        answer = ListNode()
+        curr = answer
+        while list1 and list2:
+            curr = curr.next if curr.next else curr
+            if list1.val > list2.val:
+                curr.val = list2.val
+                list2 = list2.next 
+            else:
+                curr.val = list1.val
+                list1 = list1.next 
+            curr.next = ListNode()
+            
+        curr.next = list2 if list2 else list1
+        return answer
+            
