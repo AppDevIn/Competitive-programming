c176724c626d65eca6844458ed352ebb37a1ebc7
AppDevIn <teamprojectlive@gmail.com>
2023-04-27 08:28:21 +0800

    Updated code

--- Leetcode-learn/linkedList/hasCycle.py
+++ Leetcode-learn/linkedList/hasCycle.py
@@ -1,18 +1,19 @@
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        
-        if not head or not head.next or not head.next.next:
+        if not head or not head.next:
             return False
         
-        pointer1 = head.next
-        pointer2 = head.next.next
+        slow = head
+        fast = head.next
         
-        while pointer1.next and pointer2.next and pointer2.next.next:
-            if pointer1.val == pointer2.val and pointer1.next == pointer2.next:
+        while fast and fast.next:
+            if slow.next == fast.next:
                 return True
-            pointer1 = pointer1.next
-            pointer2 = pointer2.next.next
+            slow = slow.next
+            fast = fast.next.next
             
-        return pointer1.val == pointer2.val and pointer1.next == pointer2.next
+        return False   
+
+
             
         
