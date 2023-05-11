46be01bdd3dbd5b417fff3e736933d06076e1f79
Jeyavishnu <34540492+AppDevIn@users.noreply.github.com>
2023-05-11 10:00:26 +0800

    Update level-order-traversal.py

--- Leetcode-learn/binary-tree/level-order-traversal.py
+++ Leetcode-learn/binary-tree/level-order-traversal.py
@@ -1,45 +1,36 @@
-# Definition for a binary tree node.
+Definition for a binary tree node.
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
-
-
+        
 class Solution:
-    def levelOrder(self, root: Optional[TreeNode]) -> List[int]:
-        hashSet = self.rcur(root, 0) if root is not None else []
-        if not hashSet:
-            return []
-        value = []
-        for i in range(len(hashSet.keys())):
-            value.append(hashSet.get(i))
-        return value
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root: return []
+        answer = [[root.val]]
+        queue = [[root]]
+        curr = queue[0]
+        tmp = []
+        
+        while answer[-1] != []:
+            vals = []
+            for node in curr:
+                if node.left:
+                    tmp.append(node.left)
+                    vals.append(node.left.val)
 
-    def rcur(self, node: TreeNode, depth) -> {}:
-        left_lst, right_lst = {}, {}
-        if node.left:
-            left_lst = self.rcur(node.left, depth + 1)
-        if node.right:
-            right_lst = self.rcur(node.right, depth + 1)
-
-        hashSet = {}
-
-        for k, v in left_lst.items():
-            arr = hashSet.get(k, [])
-            arr += v
-            hashSet[k] = arr
-
-        for k, v in right_lst.items():
-            arr = hashSet.get(k, [])
-            arr += v
-            hashSet[k] = arr
-
-        arr = []
-        if node.val is not None:
-            arr = hashSet.get(depth, [])
-            arr.append(node.val)
-        hashSet[depth] = arr
-
-        return hashSet
-
+                if node.right:    
+                    tmp.append(node.right)
+                    vals.append(node.right.val)
+        
+                    
+            queue.append(tmp)
+            answer.append(vals)
+            curr = tmp
+            tmp = []
+        return answer[0:-1]
+            
+        
+            
+        
