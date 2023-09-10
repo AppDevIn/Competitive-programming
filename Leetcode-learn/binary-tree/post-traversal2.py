67f076a4855c36e8070f1e2e314ef64dca798e0d
AppDevIn <teamprojectlive@gmail.com>
2023-09-10 08:28:38 +0800

    Binary Tree Preorder Traversal Part 2

---
+++ Leetcode-learn/binary-tree/post-traversal2.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        l = []
+        def df(res, root):
+            if root == None:
+                return
+            res.append(root.val)
+            df(res,root.left)
+            df(res,root.right)
+            
+            
+            
+        
+        
+        df(l, root)
+        return l
